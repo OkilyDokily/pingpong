7274208f5b62e4e24eaee302c59cccb54625454a
me <me@me.me.com>
2020-07-24 14:52:07 -0700

    realizes final spec

--- Models/PingPong.cs
+++ Models/PingPong.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace PingPong.Models{
   public class PingAndOrPong{
-    private static List<string> _string = new List<string>();
+    
     private static bool isDivisibleByThreeOrFive(int number){
       return ((number % 3) == 0) || ((number % 5) == 0);
     }
@@ -32,13 +32,11 @@ namespace PingPong.Models{
     }
 
     public static List<string> createListWithInputNumber(int number){
+      List<string> stringReturn = new List<string>();
       for(int i = 0; i <= number; i++){
-        _string.Add(returnPingOrPongOrPingPong(i));
+        stringReturn.Add(returnPingOrPongOrPingPong(i));
       }
-
-      return _string;
+      return stringReturn;
     }
-
   }
-
 }
